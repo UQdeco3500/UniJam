4acd52e0461b5ece05c7fce8e66feb1e0617552a
田辺　梨桜 <rio0618@outlook.com>
2023-10-10 01:00:22 +1000

    added map screen

---
+++ proof-of-concept/map.js
@@ -0,0 +1,45 @@
+let map, infoWindow;
+
+function displayMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -27.49708,
+            lng: 153.01364,
+        }, 
+        zoom: 17,
+    });
+    infoWindow = new google.maps.InfoWindow();
+
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Current location");
+                infoWindow.open(map);
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            },
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation ? "Error: The Geolocation service failed." : "Error: Your browser doesn't support geolocation",
+    );
+    infoWindow.open(map);
+}
+
+window.displayMap = displayMap;
