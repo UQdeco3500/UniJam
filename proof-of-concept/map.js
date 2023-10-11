691d34556bd7322190c35e8ced6c1ee52983661f
Yuhsuan Wu <yuhsuanwu@d-i89-218-192.student.eduroam.uq.edu.au>
2023-10-11 11:23:24 +1000

    implement toggle button of filter

--- proof-of-concept/map.js
+++ proof-of-concept/map.js
@@ -104,4 +104,21 @@ function heatMapData() {
 
 window.displayMap = displayMap;
 
+// onclick filter
+function onclickFunction() {
+
+    var element = document.getElementById("unselect-filter");
+    // console.log("element:", element.classList);
+    element.classList.toggle("filter-normal");
+
+  }
+
+function activeFunction() {
+
+    var element = document.getElementById("actived-filter");
+    // console.log("element:", element.classList);
+    element.classList.remove("filter-normal");
+    element.classList.toggle("filter-press");
+
+  }
 
