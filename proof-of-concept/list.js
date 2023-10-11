cc0d862365214bf22b1776b90f1748c5ff5131a5
DEEPANSHI-CHAUHAN <chauhandeepanshi@gmail.com>
2023-10-11 11:26:03 +1000

    List.js file for scripting and data manipulation

---
+++ proof-of-concept/list.js
@@ -0,0 +1,18 @@
+//script.js
+/*document.addEventListener("DOMContentLoaded", function () {
+    const boxList = document.getElementById("boxList");
+
+    // Function to add a new rectangular box to the list
+    function addBox(text) {
+        const box = document.createElement("div");
+        box.className = "box";
+        box.textContent = text;
+        boxList.appendChild(box);
+    }
+
+    // Example: Add new rectangular boxes to the list
+    addBox("Box 1");
+    addBox("Box 2");
+
+    // You can call the addBox function to add more boxes dynamically
+});*/
