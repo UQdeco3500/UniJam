1382e9cd7db412c9b755fac759c0adfd1de34aeb
Yuhsuan Wu <yuhsuanwu@d-i89-218-192.student.eduroam.uq.edu.au>
2023-10-12 13:43:56 +1000

    Fix map view bug

--- proof-of-concept/map.js
+++ proof-of-concept/map.js
@@ -20,25 +20,12 @@ const gradient = [
 ];
 
 function activeFunction() {
-  const currentUrl = new URL(window.location.href);
-  const params = new URLSearchParams(currentUrl.search);
 
+    const currentUrl = new URL(window.location.href);
+    console.log(currentUrl);
 
-  let currentEventsFilter = document.getElementById("current-events");
-  let upcomingEventsFilter = document.getElementById("upcoming-events");
-
-  const currentUrl = new URL(window.location.href);
-  console.log(currentUrl);
-
-
-  if (currentEventsFilter.checked) {
-    currentUrl.searchParams.set("filter", "current-events");
-  } else if (upcomingEventsFilter.checked) {
-    currentUrl.searchParams.set("filter", "upcoming-events");
-  }
-  window.history.replaceState(null, null, currentUrl);
-
-  // if (document.URL)
+    let currentEventsFilter = document.getElementById("current-events");
+    let upcomingEventsFilter = document.getElementById("upcoming-events");
 
     if (currentEventsFilter.checked) {
         currentUrl.searchParams.set('filter', 'current-events');
@@ -62,91 +49,63 @@ function setHeatMap() {
     heatmap.set("radius", 80);
     heatmap.set("opacity", 0.7);
     heatmap.setMap(map);
-
 }
 
-// code retrieved from: https://developers.google.com/maps/documentation/javascript/geolocation
+
+// code retrieved from: https://developers.google.com/maps/documentation/javascript/geolocation 
 let map, infoWindow, marker, heatmap;
 
 /**
- * Function to display the map screen.
+ * Function to display the map screen. 
  */
 function displayMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -27.49708,
-      lng: 153.01364,
-    },
-    zoom: 17,
-    disableDefaultUI: true,
-  });
-  infoWindow = new google.maps.InfoWindow();
-
-  // Try HTML5 geolocation
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-
-        infoWindow.setPosition(pos);
-        map.setCenter(pos);
-
-        // add current location marker --------------------
-        marker = new google.maps.Marker({
-          position: pos,
-          map: map,
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 10,
-            fillOpacity: 1,
-            strokeWeight: 2,
-            fillColor: "#5384ED",
-            strokeColor: "#ffffff",
-          },
-        });
-        // -----------------------------------------------
-      },
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
-
-    // set heatmap --------------------------------------------
-    heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatMapData(window.location.href),
-      map: map,
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -27.49708,
+            lng: 153.01364,
+        }, 
+        zoom: 17,
+        disableDefaultUI: true,
     });
+    infoWindow = new google.maps.InfoWindow();
 
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
 
-    const gradient = [
-      "rgba(0, 255, 255, 0)",
-      "rgba(0, 255, 255, 1)",
-      "rgba(0, 191, 255, 1)",
-      "rgba(0, 127, 255, 1)",
-      "rgba(0, 63, 255, 1)",
-      "rgba(0, 0, 255, 1)",
-      "rgba(0, 0, 223, 1)",
-      "rgba(0, 0, 191, 1)",
-      "rgba(0, 0, 159, 1)",
-      "rgba(0, 0, 127, 1)",
-      "rgba(63, 0, 91, 1)",
-      "rgba(127, 0, 63, 1)",
-      "rgba(191, 0, 31, 1)",
-      "rgba(255, 0, 0, 1)",
-    ];
+                infoWindow.setPosition(pos);
+                map.setCenter(pos);
 
-    heatmap.set("gradient", gradient);
-    heatmap.set("radius", 80);
-    heatmap.set("opacity", 0.7);
-    heatmap.setMap(map);
-    // --------------------------------------------------------
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+                // add current location marker --------------------
+                marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        scale: 10,
+                        fillOpacity: 1,
+                        strokeWeight: 2,
+                        fillColor: '#5384ED',
+                        strokeColor: '#ffffff',
+                    }
+                });
+                // -----------------------------------------------
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            },
+        );
+        
+        // set heatmap --------------------------------------------
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatMapData(window.location.href),
+            map: map,
+        });
 
         heatmap.set("gradient", gradient);
         heatmap.set("radius", 80);
@@ -158,68 +117,18 @@ function displayMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
-
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation"
-  );
-  infoWindow.open(map);
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation ? "Error: The Geolocation service failed." : "Error: Your browser doesn't support geolocation",
+    );
+    infoWindow.open(map);
 }
 
 // heat map data
 function heatMapData(url) {
-
-  var currentEvents = [
-    // School of Chemistry
-    new google.maps.LatLng(-27.499808273879893, 153.01286430696973),
-    new google.maps.LatLng(-27.499808273879893, 153.01286430696973),
-    // General Purpose North
-    new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-    new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-    new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-    new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-  ];
-
-  var upcomingEvents = [
-    // UQ
-    new google.maps.LatLng(-27.49708, 153.01364),
-    new google.maps.LatLng(-27.49708, 153.01364),
-    // Advanced Engineering
-    new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-    new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-    new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-    new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-  ];
-
-  if (url.includes("filter=current-events")) {
-    return currentEvents;
-  } else if (url.includes("filter=upcoming-events")) {
-    return upcomingEvents;
-  }
-
-  // return [
-  //     // UQ
-  //     new google.maps.LatLng(-27.49708, 153.01364),
-  //     new google.maps.LatLng(-27.49708, 153.01364),
-  //     // General Purpose North
-  //     new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-  //     new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-  //     new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-  //     new google.maps.LatLng(-27.49488260229967, 153.01347467277384),
-  //     // Advanced Engineering
-  //     new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-  //     new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-  //     new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-  //     new google.maps.LatLng(-27.49938954223438, 153.0147954974887),
-  //     // School of Chemistry
-  //     new google.maps.LatLng(-27.499808273879893, 153.01286430696973),
-  //     new google.maps.LatLng(-27.499808273879893, 153.01286430696973),
-  // ];
 
     var currentEvents = [
         // School of Chemistry
@@ -268,11 +177,12 @@ function heatMapData(url) {
     //     new google.maps.LatLng(-27.499808273879893, 153.01286430696973),
     //     new google.maps.LatLng(-27.499808273879893, 153.01286430696973),
     // ];
-
 }
 // ---End of code retrieved from https://developers.google.com/maps/documentation/javascript/geolocation---
 
+
 window.displayMap = displayMap;
+
 
 // onclick filter
 // function onclickFunction() {
